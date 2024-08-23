7864b87b0956985eababadc7aefdb164673fc26e
jdeiss9 <jdeiss1001@gmail.com>
2024-08-23 15:45:25 -0400

    Finished most of User Controller (Jeff)

--- application-portal-user/src/main/java/com/adp/application_portal_user/controller/UserController.java
+++ application-portal-user/src/main/java/com/adp/application_portal_user/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.adp.application_portal_user.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.adp.application_portal_user.models.User;
 import com.adp.application_portal_user.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("api/users")
@@ -25,4 +33,23 @@ public class UserController {
         User createdUser = userService.createUser(user);
         return ResponseEntity.ok(createdUser);
     }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<User>> getUserById(@PathVariable String id) {
+        Optional<User> user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<Optional<User>> getUserByEmail(@PathVariable String email) {
+        Optional<User> user = userService.getUserByEmail(email);
+        return ResponseEntity.ok(user);
+    }
+
 }
